81a9c8fa616d9f159b9581f74d3f9d0dbb31f428
profVinifs <viniciusfsantos@prof.educacao.sp.gov.br>
2024-11-03 12:24:59 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(200, 200);
+  background(220);
+  
+  translate(0,200)
+  
+  var altura = [random(-10, -100), random(-10, -100), random(-10, -100), random(-10, -100)]
+  
+  rect(24, 0, 20, altura[0])
+  rect((24+20+24), 0, 20, altura[1])
+  rect((24+20+24+20+24), 0, 20, altura[2])
+  rect((24+20+24+20+24+20+24), 0, 20, altura[3])
+}
+
